b86be95e6f3e3e99c8cde22280e2d133f249bedd
Wasley Carvalho <88601440+wasleyfps@users.noreply.github.com>
2024-09-15 23:07:12 -0300

    learning types primitives

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -1,17 +1,23 @@
 package academy.devdojo.maratonajava.introducao;
 
+import javax.lang.model.element.VariableElement;
+
 public class Aula02TiposPrimitivos {
     public static void main(String[] args){
         // int, double, float, char, byte, short, long e boolean
         int age = 26;
-        long numberLong = 100000; // -9223372036854775808 a 9223372036854775807
-        double salary = 2000; // 1.7E-308 a 1.7E+308
-        float salary2 = 2500; // 3.4E-038 a 3.4E+038
-        byte age2 = 27; // -128 a 127
-        short age3 = 10; // -32768 a 32767
+        long numberLong = 100000L; // -9223372036854775808 a 9223372036854775807
+        double salaryDouble = 2000.0; // 1.7E-308 a 1.7E+308
+        float salaryFloat = 2500.0F; // 3.4E-038 a 3.4E+038
+        byte ageByte = 127; // -128 a 127
+        short ageShort = 32000; // -32768 a 32767
         boolean isTrue = true;
         boolean isFalse = false;
-        char character = 'W';
-        System.out.println(age); 
+        char character = 'W'; // 2 bytes
+        
+        String name = "Wasley Carvalho";
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(character);
     }
 }
---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicios.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicios {
+    public static void main(String[] args){
+
+        String name = "Wasley Carvalho";
+        String address = "Rua Fernando de Noronha";
+        double salary = 1000;
+        String dateSalary = "20/12/2021";
+        String report = "Eu " +name+ ", morando no endereço " +address+" confirmo que recebi o salário de R$: "+salary+" no dia " + dateSalary;
+        
+        System.out.println(report);
+    }
+    
+    
+    
+}
