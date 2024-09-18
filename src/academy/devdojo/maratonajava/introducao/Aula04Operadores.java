7341a093bf786cbe66a051ec9244a8c733756ace
Wasley <88601440+wasleyfps@users.noreply.github.com>
2024-09-18 09:14:08 -0300

    update files java

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int numero01 = 10;
+        int numero02 = 20;
+        
+        System.out.println(numero01 + numero02);
+        System.out.println(numero01 - numero02);
+        System.out.println(numero01 * numero02);
+        
+    }
+}
