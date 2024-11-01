ce7a82db0c638e4495a3861e79e2e8923628d673
Wasley Carvalho <88601440+wasleyfps@users.noreply.github.com>
2024-11-01 12:30:41 -0300

    Orientação a Objeto

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Estudante.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Estudante.java
@@ -4,4 +4,10 @@ public class Estudante {
     public String nome;
     public int idade;
     public char sexo;
+    
+    public void imprime(){
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Idade: " + this.idade);
+        System.out.println("Sexo: " + this.sexo);
+    }
 }
