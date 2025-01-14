a58a58c0867117023c8f860443b96e3443915439
Wasley Carvalho <88601440+wasleyfps@users.noreply.github.com>
2025-01-13 23:32:01 -0300

    Learning Java is back.

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Local.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
+
+public class Local {
+    private String address;
+    
+    public Local(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
