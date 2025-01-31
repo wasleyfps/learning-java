ba1fe2d7ce2ca58b92450030ec5f19e9590d4b53
Wasley Carvalho <88601440+wasleyfps@users.noreply.github.com>
2025-01-31 15:07:46 -0300

    Leaning Number Format in Java

--- src/academy/devdojo/maratonajava/javacore/Rdates/test/NumberFormatTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Rdates/test/NumberFormatTest02.java
@@ -1,35 +1,32 @@
 package academy.devdojo.maratonajava.javacore.Rdates.test;
 
 import java.text.NumberFormat;
-import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberFormatTest01 {
+public class NumberFormatTest02 {
     public static void main(String[] args) {
-        Locale localeDefault = Locale.getDefault();
         Locale localeBR = new Locale("pt", "BR");
         Locale localeJP = new Locale("ja", "JP");
         Locale localeCH = new Locale("it", "CH");
-        
+
         NumberFormat[] numberFormats = new NumberFormat[4];
-        numberFormats[0] = NumberFormat.getInstance();
-        numberFormats[1] = NumberFormat.getInstance(localeBR);
-        numberFormats[2] = NumberFormat.getInstance(localeJP);
-        numberFormats[3] = NumberFormat.getInstance(localeCH);
-        
-        double valor = 1_000.2130;
+        numberFormats[0] = NumberFormat.getCurrencyInstance();
+        numberFormats[1] = NumberFormat.getCurrencyInstance(localeBR);
+        numberFormats[2] = NumberFormat.getCurrencyInstance(localeJP);
+        numberFormats[3] = NumberFormat.getCurrencyInstance(localeCH);
+
+        double valor = 1000.2130;
         for (NumberFormat numberFormat : numberFormats) {
-            numberFormat.setMaximumFractionDigits(2);
+            System.out.println(numberFormat.getMaximumFractionDigits());
             System.out.println(numberFormat.format(valor));
         }
         
-        String valueString = "1000,2130";
+        String valueString = "$1,000.21";
+        
         try {
             System.out.println(numberFormats[0].parse(valueString));
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-              
-
     }
 }
