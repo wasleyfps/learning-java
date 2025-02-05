f1c8c208ce3b736fdb87669631e50b640acf1133
Wasley Carvalho <88601440+wasleyfps@users.noreply.github.com>
2025-02-04 22:49:08 -0300

    Learning Java - Pattern and Matcher

--- src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest02.java
@@ -3,11 +3,18 @@ package academy.devdojo.maratonajava.javacore.Uregex.test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest01 {
+public class PatternMatcherTest02 {
     public static void main(String[] args) {
-        String regex = "aba";
+        // \d - all digits
+        // \D - all non-digits
+        // \s - all whitespaces
+        // \S - all non-whitespaces
+        // \w - all characters
+        // \W - all non-characters
+        
+        String regex = "\\d";
         String text = "abaaba";
-        String text2 = "abababa";
+        String text2 = "@#gh51f_g4h8*f64";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text2);
         System.out.println("Text:  " + text2);
@@ -15,7 +22,7 @@ public class PatternMatcherTest01 {
         System.out.println("Regex: " + regex);
         System.out.println("Positions found:");
         while (matcher.find()) {
-            System.out.print(matcher.start() + " ");
+            System.out.print(matcher.start() + " "+ matcher.group() + "\n");
         }
                
     }
