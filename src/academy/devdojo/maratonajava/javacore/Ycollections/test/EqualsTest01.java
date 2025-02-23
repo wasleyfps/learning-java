704cdced2ba18f4c0bef21e93c88a833fde9c34d
Wasley Carvalho <88601440+wasleyfps@users.noreply.github.com>
2025-02-23 17:15:20 -0300

    Learning Java - Collections

---
+++ src/academy/devdojo/maratonajava/javacore/Ycollections/test/EqualsTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Ycollections.test;
+
+public class EqualsTest01 {
+    public static void main(String[] args) {
+//        String name = "Wasley Carvalho";
+//        String name2 = "Wasley Carvalho";
+//        System.out.println(name == name2);
+        System.out.println("Wasley Carvalho");
+    }
+}
