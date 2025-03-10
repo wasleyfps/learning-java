d621f04cd933b4b4a513cf761444c566e4faa8e2
Wasley Carvalho <88601440+wasleyfps@users.noreply.github.com>
2025-03-10 10:13:49 -0300

    Learning Java - Map, ashMap, LinkedHashMap

--- src/academy/devdojo/maratonajava/javacore/Ycollections/test/NavigableSetTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycollections/test/NavigableSetTest01.java
@@ -14,6 +14,13 @@ class SmartphoneTestComparator implements Comparator<Smartphone> {
     };
 }
 
+class MangaPriceComparator implements Comparator<Manga>{
+
+    @Override
+    public int compare(Manga o1, Manga o2) {
+        return Double.compare(o1.getPrice(), o2.getPrice());
+    }
+}
 
 public class NavigableSetTest01 {
     public static void main(String[] args) {
@@ -24,15 +31,32 @@ public class NavigableSetTest01 {
         System.out.println("-------------------");
         
         NavigableSet<Manga> mangas = new TreeSet<>();
-        mangas.add(new Manga(5L,"Attack on Titan", 19.9,0));
+        mangas.add(new Manga(5L,"Attack on Titan",19.9,0));
         mangas.add(new Manga(1L,"Berserk",9.5,5));
         mangas.add(new Manga(4L,"Saitama",11.20,10));
         mangas.add(new Manga(2L,"Pokemon",2.99,3));
         mangas.add(new Manga(3L,"Solo Leveling",5.20,7));
         mangas.add(new Manga(3L,"Solo Leveling",5.20,7));
+        mangas.add(new Manga(10L, "One Piece", 3.99, 0));
         
         for (Manga manga : mangas) {
             System.out.println(manga);
         }
+        
+        Manga Jujutsu = new Manga(21L,"Jujutsu",8.0,5);
+        
+        // lower <
+        // floor <=
+        // higher >
+        // ceiling >=
+        System.out.println("-------------------------------------");
+        System.out.println(mangas.lower(Jujutsu));
+        System.out.println(mangas.floor(Jujutsu));
+        System.out.println(mangas.higher(Jujutsu));
+        System.out.println(mangas.ceiling(Jujutsu));
+
+        System.out.println(mangas.size());
+        System.out.println(mangas.pollFirst());
+        System.out.println(mangas.pollLast());
     }
 }
