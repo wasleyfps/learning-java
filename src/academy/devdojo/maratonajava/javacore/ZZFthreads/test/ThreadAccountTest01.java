95bda76ebebbcaa00864281be0227ea060311196
Wasley Carvalho <88601440+wasleyfps@users.noreply.github.com>
2025-03-23 22:57:23 -0300

    Learning Java - ThreadSafe

--- src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadAccountTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadAccountTest01.java
@@ -3,7 +3,7 @@ package academy.devdojo.maratonajava.javacore.ZZFthreads.test;
 import academy.devdojo.maratonajava.javacore.ZZFthreads.domain.Account;
 
 public class ThreadAccountTest01 implements Runnable {
-    private Account account = new Account();
+    private final Account account = new Account();
     public static void main(String[] args) {
         ThreadAccountTest01 threadAccountTest01 = new ThreadAccountTest01();
         Thread t1 = new Thread(threadAccountTest01, "Wasley");
@@ -21,14 +21,29 @@ public class ThreadAccountTest01 implements Runnable {
             }
         }
     }
-
+    private static void print(){
+        synchronized (ThreadAccountTest01.class){
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
+    
     public void withdrawal(int amount){
-        if (account.getBalance() >= 10) {
-            System.out.println(Thread.currentThread().getName() + " Cash Out ");
-            account.withdrawal(amount);
-            System.out.println(Thread.currentThread().getName() + " Completed the Withdrawal, Current Value: " + account.getBalance());
-        } else {
-            System.out.println("No money for " + Thread.currentThread().getName() + " make the withdrawal " + account.getBalance());
+        System.out.println("******* Outside Synchronized " + Thread.currentThread().getName());
+        synchronized (account) {
+            System.out.println("******* Inside Synchronized " + Thread.currentThread().getName());
+            
+            if (account.getBalance() >= 10) {
+                System.out.println(Thread.currentThread().getName() + " is going to withdraw money ");
+                account.withdrawal(amount);
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + " Completed the Withdrawal, Current Value: " + account.getBalance());
+            } else {
+                System.out.println("No money for " + Thread.currentThread().getName() + " make the withdrawal " + account.getBalance());
+            }
         }
     }
 }
