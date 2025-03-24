e7b72e2d537e06e02116f372799fa62af7a97c47
Wasley Carvalho <88601440+wasleyfps@users.noreply.github.com>
2025-03-24 15:27:03 -0300

    Learning Java - Wait, Notify, Notifyall

---
+++ src/academy/devdojo/maratonajava/javacore/ZZFthreads/service/EmailDeliveryService.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.ZZFthreads.service;
+
+import academy.devdojo.maratonajava.javacore.ZZFthreads.domain.Members;
+
+public class EmailDeliveryService implements Runnable{
+    private final Members members;
+    
+    public EmailDeliveryService(Members members){
+        this.members = members;
+    }
+    
+    @Override
+    public void run() {
+        String threadName = Thread.currentThread().getName();
+        System.out.println(threadName + "preparing to run and deliver emails");
+        while (members.isOpen() || members.pendingEmails() > 0){
+            try{
+                String email = members.retrieveEmail();
+                if(email == null) continue;
+                System.out.println(threadName + " sending email to " + email);
+                Thread.sleep(2000);
+                System.out.println(threadName);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    
+}
