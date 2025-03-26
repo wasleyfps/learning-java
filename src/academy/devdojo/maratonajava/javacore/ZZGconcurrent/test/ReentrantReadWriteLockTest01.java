544d030f46fd4ba49335633da743b564b611ad6b
Wasley Carvalho <88601440+wasleyfps@users.noreply.github.com>
2025-03-25 22:39:26 -0300

    Learning Java - ReentrantReadWriteLock

--- src/academy/devdojo/maratonajava/javacore/ZZGconcurrent/test/ReentrantReadWriteLockTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcurrent/test/ReentrantReadWriteLockTest01.java
@@ -7,60 +7,62 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 class MapReadWrite {
     private final Map<String, String> map = new LinkedHashMap<>();
-    private final ReentrantReadWriteLock reentrantReadWriteLock;
+    private final ReentrantReadWriteLock rwl;
 
-    MapReadWrite(ReentrantReadWriteLock reentrantReadWriteLock) {
-        this.reentrantReadWriteLock = reentrantReadWriteLock;
+    MapReadWrite(ReentrantReadWriteLock rwl) {
+        this.rwl = rwl;
     }
 
     public void put(String key, String value) {
-        reentrantReadWriteLock.readLock().lock();
+        rwl.writeLock().lock();
         try {
             map.put(key, value);
+            System.out.printf("Adding key-value %s - %s ", key, value);
             Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
+
         } finally {
-            reentrantReadWriteLock.writeLock().unlock();
+            rwl.writeLock().unlock();
         }
     }
 
     public Set<String> allKeys() {
-        reentrantReadWriteLock.readLock().lock();
+        rwl.readLock().lock();
         try {
             return map.keySet();
         } finally {
-            reentrantReadWriteLock.readLock().unlock();
+            rwl.readLock().unlock();
         }
     }
 }
 
+
 public class ReentrantReadWriteLockTest01 {
     public static void main(String[] args) {
-        ReentrantReadWriteLock reentrantReadWriteLock1 = new ReentrantReadWriteLock();
-        MapReadWrite mapReadWrite = new MapReadWrite(reentrantReadWriteLock1);
+        ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+        MapReadWrite mapReadWrite = new MapReadWrite(rwl);
         Runnable writer = () -> {
             for (int i = 0; i < 20; i++) {
                 mapReadWrite.put(String.valueOf(i), String.valueOf(i));
+
             }
         };
         Runnable reader = () -> {
-            if (reentrantReadWriteLock1.isWriteLocked()) {
-                System.out.println("Write Locked!");
+            if (rwl.isWriteLocked()) {
+                System.out.println("WRITE LOCKED!");
             }
-            reentrantReadWriteLock1.readLock().lock();
-            System.out.println("Finnaly I've got the damn read lock");
+            rwl.readLock().lock();
+            System.out.println("FINALLY I'VE GOT THE DAMN READ LOCK");
             try {
-                System.out.println(Thread.currentThread().getName() + mapReadWrite.allKeys());
+                System.out.println(Thread.currentThread().getName() + "  " + mapReadWrite.allKeys());
             } finally {
-                reentrantReadWriteLock1.readLock().unlock();
+                rwl.readLock().unlock();
             }
         };
-
         Thread thread1 = new Thread(writer);
         Thread thread2 = new Thread(reader);
         Thread thread3 = new Thread(reader);
-
         thread1.start();
         thread2.start();
         thread3.start();
