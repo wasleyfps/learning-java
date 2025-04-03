70a1ca078e315eaaf83bd316dcd80cdb07c729ed
Wasley Carvalho <88601440+wasleyfps@users.noreply.github.com>
2025-04-02 21:30:21 -0300

    Learning Java - JDBC

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
+
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
+
+public class ConnectionFactoryTest01 {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
